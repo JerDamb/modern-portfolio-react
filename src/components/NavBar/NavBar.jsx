b637f11b0ead0c7615fc6d3abfb5b8a2c327edd9
JerDamb <gaufre.mr@gmail.com>
2022-10-20 15:04:34 +0200

    Navbar is done, maybe a little bug for 1 frame in smartphone mode

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -7,7 +7,7 @@ import { images } from '../../constants'
 
 const NavBar = () => {
 
-  const [toggle, setToggle] = useState(false);
+  const [toggle, setToggle] = useState(true);
 
   return (
     <nav className="app__navbar">
@@ -24,6 +24,24 @@ const NavBar = () => {
       </ul>
 
       <div className="app__navbar-menu">
+      <HiMenuAlt4 onClick={() => setToggle(true)} /> 
+          {toggle && (
+            <motion.div
+              whileInView={{ x: [300, 0] }}
+              transition={{ duration: 0.85, ease: 'easeOut' }}
+            >
+              <HiX onClick={() => setToggle(false)} />
+              <ul>
+                {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
+                  <li key={item}>
+                    <a href={`#${item}`} onClick={() => setToggle(false)}>
+                      {item}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </motion.div>
+          )}
       </div>
     </nav>
   );
