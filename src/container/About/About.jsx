78640a6adca7b4bf12482a8a76ae7abb5b77b0f3
JerDamb <gaufre.mr@gmail.com>
2022-11-15 14:00:44 +0100

    beginning of about section

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './About.scss'
+import { motion } from "framer-motion";
+
+const abouts = [
+  { title: 'web development', description: 'I am a good webdeveloper', imgUrl:''},
+  { title: 'web designer', description: 'I am a good webdeveloper', imgUrl:''},
+  { title: 'UI/UX', description: 'I am a good webdeveloper', imgUrl:''},
+  { title: 'web animations', description: 'I am a good webdeveloper', imgUrl:''}
+]
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+    <h2 className='head-text'>I know that 
+      <span>Good design</span>
+      <br />
+      means
+      <span>Good business</span>
+      </h2>
+      <div className="app__profiles">
+      {abouts.map((about, index) =>(
+        <motion.div
+         whileInView={{opacity:1 }}
+         whileHover={{scale:1.1}}
+         transition={{ duration: 0.5, type:"tween"}}
+         className="app__profile-item"
+         key={about.title + index}
+        >
+          <img src={about.imgUrl} alt={about.title} />
+          <h2 className='bold-text' style={{marginTop: 20}}>{about.title}</h2>
+          <p className='p-text' style={{marginTop:10}}>{about.description}</p>
+        </motion.div>
+      ))}
+      </div>
+    </>
   )
 }
 
